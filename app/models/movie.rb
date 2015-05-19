47139477461fd1a9ef69c1091931a16835cab2f2
Juliana Davila <judaro13@gmail.com>
2015-05-19 16:43:25 -0500

    dump and fix the code

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,23 +20,21 @@ class Movie
 
   def fix_name
     fname = self.name
+    fname = fname.gsub!(/[!@%:"]/,'')
     fname = fname.gsub('&', 'and')
-    fname = fname.gsub(".", "")
-    fname = fname.gsub("!", "")
-    fname = fname.gsub("'", "")
-    fname = fname.gsub(":", "")
-    fname = fname.gsub("/", "")
-    fname = fname.gsub("?", "")
     fname = fname.gsub("+", ".")
     fname = I18n.transliterate(fname)
     self.name = fname
     self.save
   end
 
-  def fix
+  def enrich
+    filter_q1 = "FILTER (?s = <http://dbpedia.org/resource/#{film_name}>)"
+    filter_q2 = "FILTER (?s = <http://dbpedia.org/resource/#{film_name}_(film)>)" 
+    filter_q3 = "FILTER (?s = <http://dbpedia.org/resource/#{film_name}_(#{self.release_year.to_s}_film)>)"
 
-    [query1, query2, query3, query4].each do |q|
-
+    [filter_q1, filter_q2, filter_q3].each do |f|
+      q = query(f)
       sparql = SPARQL::Client.new("http://dbpedia.org/sparql")
       result = sparql.query(q)
       if !result.empty? 
@@ -59,150 +57,34 @@ class Movie
   end
 
   def set_results(result)
-    music = Set.new
-    director = Set.new
-    editing = Set.new
-    producer = Set.new
-    starring = Set.new
-    writer = Set.new
-    narrator = Set.new
-    cinema = Set.new
-
     result.each do |re|
-      if re.bound?(:music)
-        m = re.music.to_s.gsub('http://dbpedia.org/resource/', '')
-        m = m.split("_")
-        m.each{|s| m.delete(s) if s.include?('(')}
-        m = m.join(' ')
-        music << m
-      end
+      [:music, :director, :producer, :starring, :writer, :narrator, :cinema].each do |item|
+        data = re[item].to_s.gsub('http://dbpedia.org/resource/', '')
+        next unless data
+        data = data.split("_")
+        data = data.join(' ')
 
-      if re.bound?(:director)
-        m = re.director.to_s.gsub('http://dbpedia.org/resource/', '')
-        m = m.split("_")
-        m.each{|s| m.delete(s) if s.include?('(')}
-        m = m.join(' ')
-        director << m
-      end
-
-      if re.bound?(:editing)
-        m = re.editing.to_s.gsub('http://dbpedia.org/resource/', '')
-        m = m.split("_")
-        m.each{|s| m.delete(s) if s.include?('(')}
-        m = m.join(' ')
-        editing << m
-      end
-
-      if re.bound?(:producer)
-        m = re.producer.to_s.gsub('http://dbpedia.org/resource/', '')
-        m = m.split("_")
-        m.each{|s| m.delete(s) if s.include?('(')}
-        m = m.join(' ')
-        producer << m
-      end
-
-      if re.bound?(:starring)
-        m = re.starring.to_s.gsub('http://dbpedia.org/resource/', '')
-        m = m.split("_")
-        m.each{|s| m.delete(s) if s.include?('(')}
-        m = m.join(' ')
-        starring << m
-      end
-
-      if re.bound?(:writer)
-        m = re.writer.to_s.gsub('http://dbpedia.org/resource/', '')
-        m = m.split("_")
-        m.each{|s| m.delete(s) if s.include?('(')}
-        m = m.join(' ')
-        writer << m
-      end
-
-      if re.bound?(:narrator)
-        m = re.narrator.to_s.gsub('http://dbpedia.org/resource/', '')
-        m = m.split("_")
-        m.each{|s| m.delete(s) if s.include?('(')}
-        m = m.join(' ')
-        narrator << m
-      end
-
-      if re.bound?(:cinema)
-        m = re.cinema.to_s.gsub('http://dbpedia.org/resource/', '')
-        m = m.split("_")
-        m.each{|s| m.delete(s) if s.include?('(')}
-        m = m.join(' ')
-        cinema << m
+        if item == :cinema
+          self.cinematographies << data
+        elsif item == :music
+          self.music_composers << data
+        else
+          begin
+            self["#{item}s"] << data
+          rescue
+            binding.pry
+          end
+        end
       end
     end
-    self.directors = director
-    self.producers = producer
-    self.writers = writer
-    self.narrators = narrator
-    self.starrings = starring
-    self.cinematographies = cinema
-    self.music_composers = music
     self.save
   end
 
-  def query1
+  def query(filter)
     'SELECT DISTINCT ?s ?director ?editing ?producer ?starring ?writer ?music ?narrator ?cinema
               WHERE {
                 ?s <http://dbpedia.org/ontology/wikiPageID> ?id.
-                FILTER (?s = <http://dbpedia.org/resource/'+film_name+'>) 
-                OPTIONAL {?s dbpedia-owl:director ?director}.
-                OPTIONAL {?s dbpedia-owl:editing ?editing}.
-                OPTIONAL {?s dbpedia-owl:producer ?producer}.
-                OPTIONAL {?s dbpedia-owl:starring ?starring}.
-                OPTIONAL {?s dbpprop:starring ?starring}.
-                OPTIONAL {?s dbpedia-owl:writer ?writer}.
-                OPTIONAL {?s dbpprop:music ?music}.
-                OPTIONAL {?s dbpprop:narrator ?narrator}.
-                OPTIONAL {?s dbpprop:cinematography ?cinema}.
-              }'
-  end
-
-  def query2
-    'SELECT DISTINCT ?s ?director ?editing ?producer ?starring ?writer ?music ?narrator ?cinema
-              WHERE {
-                ?s <http://dbpedia.org/ontology/wikiPageID> ?id.
-                FILTER (?s = <http://dbpedia.org/resource/'+film_name+'_(film)>) 
-                OPTIONAL {?s dbpedia-owl:director ?director}.
-                OPTIONAL {?s dbpedia-owl:editing ?editing}.
-                OPTIONAL {?s dbpedia-owl:producer ?producer}.
-                OPTIONAL {?s dbpedia-owl:starring ?starring}.
-                OPTIONAL {?s dbpprop:starring ?starring}.
-                OPTIONAL {?s dbpedia-owl:writer ?writer}.
-                OPTIONAL {?s dbpprop:music ?music}.
-                OPTIONAL {?s dbpprop:narrator ?narrator}.
-                OPTIONAL {?s dbpprop:cinematography ?cinema}.
-              }'
-  end
-
-  def query3
-    'SELECT DISTINCT ?s ?director ?editing ?producer ?starring ?writer ?music ?narrator ?cinema
-              WHERE {
-                ?s <http://dbpedia.org/ontology/wikiPageID> ?id.
-                FILTER (?s = <http://dbpedia.org/resource/'+film_name+'_('+self.release_year.to_s+'_film)>) 
-                OPTIONAL {?s dbpedia-owl:director ?director}.
-                OPTIONAL {?s dbpedia-owl:editing ?editing}.
-                OPTIONAL {?s dbpedia-owl:producer ?producer}.
-                OPTIONAL {?s dbpedia-owl:starring ?starring}.
-                OPTIONAL {?s dbpprop:starring ?starring}.
-                OPTIONAL {?s dbpedia-owl:writer ?writer}.
-                OPTIONAL {?s dbpprop:music ?music}.
-                OPTIONAL {?s dbpprop:narrator ?narrator}.
-                OPTIONAL {?s dbpprop:cinematography ?cinema}.
-              }'
-  end
-
-  def query4
-    'SELECT DISTINCT ?s ?director ?editing ?producer ?starring ?writer ?music ?narrator ?cinema
-              WHERE {
-                ?s a dbpedia-owl:Film.
-                ?s rdfs:label ?label .  
-                ?label bif:contains "'+film_name+'" . 
-                ?s dcterms:subject ?sub.
-                ?s dbpprop:released ?date.
-                FILTER regex(str(?date), "'+self.release_year.to_s+'").
+                '+filter+'
                 OPTIONAL {?s dbpedia-owl:director ?director}.
                 OPTIONAL {?s dbpedia-owl:editing ?editing}.
                 OPTIONAL {?s dbpedia-owl:producer ?producer}.
